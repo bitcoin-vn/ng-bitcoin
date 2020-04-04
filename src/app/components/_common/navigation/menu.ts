2f49888f86f94596538fb73b3606b289052efe22
Duonghuudai <duonghuudai.id002@gmail.com>
2020-04-04 12:17:56 +0700

    Update icon

--- src/app/components/_common/navigation/menu.ts
+++ src/app/components/_common/navigation/menu.ts
@@ -2,81 +2,91 @@ export const Menu = [
     {
         name: 'Home',
         path: '/',
-        target: ''
+        target: '',
+        icon: ""
     },
     {
         name: 'Charts',
         path: '/charts',
-        target: ''
+        target: '',
+        icon: ""
     },
     {
         name: 'Analysis',
         path: '/analysis',
-        target: ''
+        target: '',
+        icon: ""
     },
     {
         name: 'Chat Room',
         path: '/chat-room',
-        target: ''
+        target: '',
+        icon: ""
     },
     {
         name: 'Cryptocurrency',
         path: '/cryptocurrency',
-        target: ''
-    },
-    {
-        name: 'My Notes ✰',
-        path: '/notes',
-        target: ''
+        target: '',
+        icon: ""
     },
     {
         name: 'Convert BTC to USD',
         path: '/btc-to-usd',
-        target: ''
+        target: '',
+        icon: ""
     },
     {
         name: 'Convert USD to BTC',
         path: '/usd-to-btc',
-        target: ''
+        target: '',
+        icon: ""
     },
     {
         name: 'Convert BTC to Altcoin',
         path: '/btc-to-altcoin',
-        target: ''
+        target: '',
+        icon: ""
     },
     {
         name: 'Bitcoin QR Code',
         path: '/bitcoin-qrcode',
-        target: ''
+        target: '',
+        icon: ""
     },
     {
         name: 'Bitcoin Paper Wallet',
         path: '/bitcoin-paper-wallet',
-        target: ''
+        target: '',
+        icon: ""
     },
     {
         name: 'Bitcoin Age Distribution',
         path: '/bitcoin-age-distribution',
-        target: ''
+        target: '',
+        icon: ""
     },
     {
         name: 'Google Trends',
         path: 'https://trends.google.com.vn/trends/explore?q=Bitcoin',
-        target: 'blank'
+        target: '',
+        icon: "pi-external-link"
     },
     {
         name: 'Bitcoin Frees',
         path: 'https://bitcoinfees.earn.com/',
-        target: 'blank'
+        target: '',
+        icon: "pi-external-link"
     },
     {
         name: 'Fear And Greed',
         path: 'https://alternative.me/crypto/fear-and-greed-index',
-        target: 'blank'
+        target: '',
+        icon: "pi-external-link"
     },
     {
         name: 'Binance Register',
         path: 'https://www.binance.com/vn/markets?ref=37439706',
-        target: 'blank'
+        target: '',
+        icon: "pi-external-link"
     }
 ]
